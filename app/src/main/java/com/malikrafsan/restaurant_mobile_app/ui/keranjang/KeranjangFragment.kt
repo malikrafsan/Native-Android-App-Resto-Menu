143a2b25455ecca92411aba2ac08d2f7c580e5d9
pandora-1 <michaelkahari.mk@gmail.com>
2023-02-14 02:01:45 +0700

    add text and button in keranjang page

--- app/src/main/java/com/malikrafsan/restaurant_mobile_app/ui/keranjang/KeranjangFragment.kt
+++ app/src/main/java/com/malikrafsan/restaurant_mobile_app/ui/keranjang/KeranjangFragment.kt
@@ -1,18 +1,29 @@
 package com.malikrafsan.restaurant_mobile_app.ui.keranjang
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+
 
 class KeranjangFragment : Fragment() {
 
 private var _binding = null
   // This property is only valid between onCreateView and
   // onDestroyView.
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(com.malikrafsan.restaurant_mobile_app.R.layout.fragment_keranjang, container, false)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
 
 
 override fun onDestroyView() {
