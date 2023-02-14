6d25eefa26a074ead13f9a7d07179cf6dc75ef2d
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-02-14 23:28:55 +0700

    feat: add temperature sensor

--- app/src/main/java/com/malikrafsan/restaurant_mobile_app/MainActivity.kt
+++ app/src/main/java/com/malikrafsan/restaurant_mobile_app/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.malikrafsan.restaurant_mobile_app
 
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -23,9 +29,12 @@ import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Callback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
 private lateinit var binding: ActivityMainBinding
+private lateinit var sensor: Sensor
+private lateinit var sensorManager: SensorManager
+private var isTemperatureSensorAvailable: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,50 +52,67 @@ private lateinit var binding: ActivityMainBinding
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        findViewById<Button>(R.id.pay_button).setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    ScanPaymentActivity::class.java
+                )
+            )
+        }
+
+//        sensorManager = SensorManager getSystemService(SENSOR_SERVICE)
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE) != null) {
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE)
+            isTemperatureSensorAvailable = true
+        } else {
+            findViewById<TextView>(R.id.textViewTemperature).text = "Temperature sensor is not available"
+            isTemperatureSensorAvailable = false
+        }
+
         loadMenu()
         loadFoodMenu()
         loadDrinkMenu()
         loadBranch()
-        pay()
     }
 
-    private fun pay() {
-        val paymentApi = ApiBuilder.buildApi(Payment::class.java)
-        val requestCall = paymentApi.pay("valgkhmefzopsyxfknejiahjzhhxthlo")
+    override fun onSensorChanged(event: SensorEvent?) {
+        findViewById<TextView>(R.id.textViewTemperature).text = "Temperature: ${event?.values?.get(0)} C"
+    }
 
-        requestCall.enqueue(object : Callback<PayResponse> {
-            override fun onResponse(call: Call<PayResponse>, response: Response<PayResponse>) {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        Log.d("PAYMENT", it.toString())
-                    }
-                } else {
-                    Log.d("PAYMENT", "UNSUCCESSFUL: " + response.errorBody().toString())
-                }
-            }
-            override fun onFailure(call: Call<PayResponse>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
-                Log.d("PAYMENT", "FAILED: " + t.message.toString())
-            }
-        })
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d("MainActivity", "onAccuracyChanged: $accuracy")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isTemperatureSensorAvailable) {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (isTemperatureSensorAvailable) {
+            sensorManager.unregisterListener(this)
+        }
     }
 
     private fun loadMenu() {
         val menu = ApiBuilder.buildApi(MenuApi::class.java)
         val requestCall = menu.getMenu()
 
-        val textViewMenu = this.findViewById<TextView>(R.id.textViewMenu)
         requestCall.enqueue(object : Callback<Menu> {
             override fun onResponse(call: Call<Menu>, response: Response<Menu>) {
                 if (response.isSuccessful) {
                     response.body()?.let {
-                        textViewMenu.text = it.toString()
+                        Toast.makeText(this@MainActivity, "$it", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
             override fun onFailure(call: Call<Menu>, t: Throwable) {
                 Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
-                textViewMenu.text = t.message
             }
         })
     }
@@ -95,18 +121,16 @@ private lateinit var binding: ActivityMainBinding
         val menu = ApiBuilder.buildApi(MenuApi::class.java)
         val requestCall = menu.getFoodMenu()
 
-        val textViewFoodMenu = this.findViewById<TextView>(R.id.textViewFoodMenu)
         requestCall.enqueue(object : Callback<Menu> {
             override fun onResponse(call: Call<Menu>, response: Response<Menu>) {
                 if (response.isSuccessful) {
                     response.body()?.let {
-                        textViewFoodMenu.text = it.toString()
+                        Toast.makeText(this@MainActivity, "$it", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
             override fun onFailure(call: Call<Menu>, t: Throwable) {
                 Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
-                textViewFoodMenu.text = t.message
             }
         })
     }
@@ -115,18 +139,16 @@ private lateinit var binding: ActivityMainBinding
         val menu = ApiBuilder.buildApi(MenuApi::class.java)
         val requestCall = menu.getDrinkMenu()
 
-        val textViewDrinkMenu = this.findViewById<TextView>(R.id.textViewDrinkMenu)
         requestCall.enqueue(object : Callback<Menu> {
             override fun onResponse(call: Call<Menu>, response: Response<Menu>) {
                 if (response.isSuccessful) {
                     response.body()?.let {
-                        textViewDrinkMenu.text = it.toString()
+                        Toast.makeText(this@MainActivity, "$it", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
             override fun onFailure(call: Call<Menu>, t: Throwable) {
                 Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
-                textViewDrinkMenu.text = t.message
             }
         })
     }
@@ -135,18 +157,16 @@ private lateinit var binding: ActivityMainBinding
         val branch = ApiBuilder.buildApi(BranchApi::class.java)
         val requestCall = branch.getBranch()
 
-        val textViewBranch = this.findViewById<TextView>(R.id.textViewBranch)
         requestCall.enqueue(object : Callback<Branch> {
             override fun onResponse(call: Call<Branch>, response: Response<Branch>) {
                 if (response.isSuccessful) {
                     response.body()?.let {
-                        textViewBranch.text = it.toString()
+                        Toast.makeText(this@MainActivity, "$it", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
             override fun onFailure(call: Call<Branch>, t: Throwable) {
                 Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
-                textViewBranch.text = t.message
             }
         })
     }
