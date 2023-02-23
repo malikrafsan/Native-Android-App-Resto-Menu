4e3c609f043d0fb4222424afde322e6d8631eb1a
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-02-23 19:05:43 +0700

    feat: disable night mode

--- app/src/main/java/com/malikrafsan/restaurant_mobile_app/ui/splash_screen/SplashActivity.kt
+++ app/src/main/java/com/malikrafsan/restaurant_mobile_app/ui/splash_screen/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
 import com.malikrafsan.restaurant_mobile_app.MainActivity
 import com.malikrafsan.restaurant_mobile_app.R
 
@@ -12,6 +13,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         supportActionBar?.hide()
 
